eec9a031106faebf867112010cac3f5e7685bd23
Ryan The <ryan.the.2006@gmail.com>
2020-06-07 10:17:57 +0800

    Add favicon change to dark mode

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Ryan Theodore The</title>
+        <link rel="icon" href="/favicon-dark.ico" />
+      </Head>
+    </div>
+  )
+}
