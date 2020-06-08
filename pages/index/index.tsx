6cc710454c4342cc4b8984a6d915797d3ef1b4f9
Ryan The <ryan.the.2006@gmail.com>
2020-06-08 20:25:03 +0800

    Add firebase

---
+++ pages/index/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+import style from "./style.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div className={style.main}>
+        <h1>Student.</h1>
+        <h1>Developer.</h1>
+        <h1>Designer.</h1>
+        <h1>Robotics.</h1>
+      </div>
+    </>
+  );
+}
