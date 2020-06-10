774675ae123aefe7a5ae28cf4fd2ceb22ebef1df
Ryan The <ryan.the.2006@gmail.com>
2020-06-10 10:27:13 +0800

    Add mobile style support

--- components/cards/post/index.tsx
+++ components/cards/post/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { ReactChildren, ReactNode } from "react";
 import Link from "next/link";
 
 import style from "./style.module.css";
 import Post from "../../../models/post";
 
-export default function PostCard(props: { value: Post; currentPosts: Post[] }) {
+export default function PostCard(props: { value: Post; currentPosts: Post[]}) {
   const LinkWrapper = ({ condition, wrapper, children }) =>
     condition ? wrapper(children) : children;
 
@@ -13,7 +13,7 @@ export default function PostCard(props: { value: Post; currentPosts: Post[] }) {
       condition={props.currentPosts.length}
       wrapper={(children) => (
         <Link
-          href="/works/[url]"
+          href="/works/[query]"
           as={`/works/${props.value?.title
             ?.toLowerCase()
             .replace(/[^0-9a-zA-Z-_]/g, "-") ?? "ERROR"}`}
@@ -26,7 +26,9 @@ export default function PostCard(props: { value: Post; currentPosts: Post[] }) {
         className={`${style.gridPost} ${
           !props.currentPosts.length ? style.loading : null
         }`}
-      />
+      >
+        {props.value?.media as string[]}
+      </div>
     </LinkWrapper>
   );
 }
--- pages/works/[query]/index.tsx
+++ pages/works/[query]/index.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from "react";
-import Router from 'next/router'
+import React from "react";
+import { useRouter } from "next/router";
 
 import Post from "../../../models/post";
 import style from "./style.module.css";
 
 export default function WorksUrlPage() {
 
-  useEffect(() => {
+  const query = useRouter().query.query as string;
+  const post = new Post(query);
+  console.log(post);
 
-  })
   return (
     <>
       <div className={style.main}></div>
