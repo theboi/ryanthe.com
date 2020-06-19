79cba53a8b49421ff57bacc5fa7fbfc05a79153e
Ryan The <ryan.the.2006@gmail.com>
2020-06-19 22:06:34 +0800

    Add filter dropdown/sidebar for mobile/desktop

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,12 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-import Post from "../../models/post";
+import Post, { Genre } from "../../models/post";
 import style from "./style.module.css";
 import PostCard from "../../components/cards/post";
 
-export default function WorkPage() {
+export default function WorkPage(props) {
+  const mobileMinWidth = 768;
   const [currentPosts, setCurrentPosts] = useState([]);
   const [isGrid, setIsGrid] = useState(true);
+  const [filter, setFilter] = useState(Genre.All);
+  const [mobileFilterIsOpen, setMobileFilterIsOpen] = useState(false);
+  const genreList = [
+    Genre.All,
+    Genre.Code,
+    Genre.Design,
+    Genre.Robot,
+    Genre.Others,
+  ];
+  const filterTypes = [
+    genreList[filter - 1],
+    ...genreList.filter((value, index) => index !== filter - 1),
+  ];
 
   useEffect(() => {
     Post.getPosts().then((data) => {
@@ -14,19 +28,49 @@ export default function WorkPage() {
       if (!currentPosts.length) setCurrentPosts(data);
     });
   });
-
+  
   return (
     <>
       <div className={style.main}>
-        <div className={style.filter}>
-          <ul>
-            <li>All.</li>
-            <li>Code.</li>
-            <li>Design.</li>
-            <li>Robot.</li>
-            <li>Others.</li>
-            <button onClick={() => setIsGrid(!isGrid)}>Switch</button>
-          </ul>
+        <div>
+          <div
+            className={`${style.filter} ${
+              mobileFilterIsOpen ? style.active : null
+            }`}
+            onClick={() => {
+              if (!(props.width >= mobileMinWidth)) {
+                setMobileFilterIsOpen(!mobileFilterIsOpen)
+              };
+            }}
+          >
+            <ul>
+              {(props.width >= mobileMinWidth ? genreList : [
+                genreList[filter - 1],
+                ...genreList.filter((value, index) => index !== filter - 1),
+              ]).map((value, index) => {
+                return (
+                  <FilterButton
+                    title={Post.genreToString(value)}
+                    key={index}
+                    index={index}
+                    filter={filter}
+                    onClick={() => {
+                      if (props.width >= mobileMinWidth) setFilter(genreList[index]);
+                      else if (mobileFilterIsOpen) setFilter(filterTypes[index]);
+                    }}
+                  />
+                );
+              })}
+
+            </ul>
+          </div>
+          <div onClick={() => setIsGrid(!isGrid)} className={style.switch}>
+            {isGrid ? (
+              <i className="fas fa-th-list"></i>
+            ) : (
+              <i className="fas fa-th"></i>
+            )}
+          </div>
         </div>
         <div className={`${style.content} ${isGrid ? style.grid : style.list}`}>
           {(!currentPosts.length ? [...Array(12)] : currentPosts).map(
@@ -44,3 +88,19 @@ export default function WorkPage() {
     </>
   );
 }
+
+const FilterButton = (props: {
+  title: string;
+  index: number;
+  filter: Genre;
+  onClick: () => void
+}) => {
+  return (
+    <li
+      onClick={props.onClick}
+      className={props.filter === props.index + 1 ? style.isSelected : null}
+    >
+      {props.title}
+    </li>
+  );
+};
