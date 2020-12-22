b63c77fae2b3febb2240213ed4968e3d7d834780
Ryan The <ryan.the.2006@gmail.com>
2020-12-22 23:02:27 +0800

    Remove code in progress

--- src/components/cards/post/index.tsx
+++ src/components/cards/post/index.tsx
@@ -12,32 +12,32 @@ export default function PostCard(props: {
   const [image, setImage] = useState<string | ArrayBuffer>(undefined);
 
   useEffect(() => {
-    if (props.post?.media.length) {
-      Post.getStorage(props.post.media[0])
-        .then((res) => {
-          setImage(res);
-        })
-        .catch((err) => {
-          console.error("ERROR: ", err);
-        });
-    }
+    // if (props.post?.media.length) {
+    //   Post.getStorage(props.post.media[0])
+    //     .then((res) => {
+    //       setImage(res);
+    //     })
+    //     .catch((err) => {
+    //       console.error("ERROR: ", err);
+    //     });
+    // }
   });
 
   const LinkWrapper = ({ condition, wrapper, children }) =>
     condition ? wrapper(children) : children;
 
   return (
-    <LinkWrapper
-      condition={props.currentPosts.length}
-      wrapper={(children) => (
-        <Link
-          href="/works/[query]"
-          as={`/works/${Post.parseToUrl(props.post?.title ?? "ERROR")}`}
-        >
-          <a>{children}</a>
-        </Link>
-      )}
-    >
+    // <LinkWrapper
+    //   condition={props.currentPosts.length}
+    //   wrapper={(children) => (
+    //     <Link
+    //       href="/works/[query]"
+    //       as={`/works/${Post.parseToUrl(props.post?.title ?? "ERROR")}`}
+    //     >
+    //       <a>{children}</a>
+    //     </Link>
+    //   )}
+    // >
       <div className={style.content}>
         <div
           className={`${style.post} ${props.isGrid ? style.grid : style.list} ${
@@ -50,17 +50,17 @@ export default function PostCard(props: {
           />
           <div className={style.details}>
             <p>
-              {(() => {
+              {/* {(() => {
                 return props.post?.genre
                   .map((value) => Post.genreToString(value))
                   .join(" | ");
-              })()}
+              })()} */}
             </p>
             <h2>{props.post?.title}</h2>
             <p>{props.post?.body}</p>
           </div>
         </div>
       </div>
-    </LinkWrapper>
+    // </LinkWrapper>
   );
 }
