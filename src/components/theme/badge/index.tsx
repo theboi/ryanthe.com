ba8d79735ad81fc9ef83f604a787bdfebf3cd6d7
Ryan The <ryan.the.2006@gmail.com>
2022-05-02 17:47:20 +0800

    Fix div descendant of p

--- src/components/theme/badge/index.tsx
+++ src/components/theme/badge/index.tsx
@@ -3,5 +3,5 @@ import Link from "next/link";
 import style from "./style.module.scss";
 
 export default function ThemeBadge({ children }: { children: React.ReactNode }) {
-  return <div className={style.wrapper}>{children}</div>;
+  return <span className={style.wrapper}>{children}</span>;
 }
