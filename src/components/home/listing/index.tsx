f3536830f20c2df0eb544be7c405022a944dfe74
Ryan The <ryan.the.2006@gmail.com>
2022-05-03 11:46:02 +0800

    Update home page content

--- src/components/home/listing/index.tsx
+++ src/components/home/listing/index.tsx
@@ -29,7 +29,7 @@ const data = [
     entries: [
       {
         title: "School of Science and Technology, Singapore",
-        desc: "In the School of Science and Technology, Singapore (SST), I learnt various technical and soft skills",
+        desc: "In my four years at the School of Science and Technology, Singapore (SST), I learned various soft skills—including effective collaboration, adaptability in leadership, presentation skills—and technical skills—including design and programming. SST's self-driven learning culture taught me to overcome challenges through critical and creative thinking while staying committed and resilient. In overcoming these challenges, I also learned the value of teamwork and communicating effectively with teammates. As part of the O-Level examinations, I specialised in Computing+ for my Applied Subject, which equipped me with skills for solving problems through computational thinking.",
         period: "2019-2022",
         img: "sst.png",
       },
@@ -46,7 +46,7 @@ const data = [
       },
       {
         title: "ExCo, Robotics @APEX CCA",
-        desc: "As the Head of Training within my CCA's ExCo, I started and utilised a project management platform called Notion to",
+        desc: "As a CCA leader, I started and managed a Notion workspace, which is a team management platform, for the ExCo and respective committees to keep track of the club's timeline, student list, meeting minutes, publicity and internal projects. I also started and helped run the club's Instagram page, including planning, designing and publishing posts. Specifically, as the Head of Training of my CCA, I also planned the timeline for training and preparation aligned to upcoming competitions to allow our members to anticipate and be ready for events and challenges. A reference copy of the Notion page I had started and passed down to my juniors can be found here.",
         period: "2021-2022",
         img: "roboapex.png",
       },
@@ -58,7 +58,7 @@ const data = [
       },
       {
         title: "Vice-Chair, Class S201",
-        desc: "some desc here lorem ipsum blah blah blah some desc here lorem ipsum blah blah blah some desc here lorem ipsum blah blah blah some desc here lorem ipsum blah blah blahsome desc here lorem ipsum blah blah blah some desc here lorem ipsum blah blah blah some desc here lorem ipsum blah blah blah",
+        desc: "My first opportunity to lead as a Vice-chairperson",
         period: "2020",
         img: "sst.png",
       },
