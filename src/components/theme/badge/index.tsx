bf1d5cfca8d27806a2fb3b0b3eb190dffe1744ad
Ryan The <ryan.the.2006@gmail.com>
2022-07-03 14:21:56 +0800

    Update works themebadge style

--- src/components/theme/badge/index.tsx
+++ src/components/theme/badge/index.tsx
@@ -1,7 +1,23 @@
-import Link from "next/link";
+import clsx from "clsx";
+import { WorksDiscipline } from "../../../data/works";
 
 import style from "./style.module.scss";
 
-export default function ThemeBadge({ children }: { children: React.ReactNode }) {
-  return <span className={style.wrapper}>{children}</span>;
+function getColor(d: WorksDiscipline) {
+  switch (d) {
+    case WorksDiscipline.Computing: return "blue"
+    case WorksDiscipline.Design: return "indigo"
+    case WorksDiscipline.Engineering: return "purple"
+    case WorksDiscipline.English: return "pink"
+    case WorksDiscipline.Humanities: return "red"
+    case WorksDiscipline.Innovation: return "orange"
+    case WorksDiscipline.Leadership: return "yellow"
+    case WorksDiscipline.Math: return "green"
+    case WorksDiscipline.Robotics: return "teal"
+    case WorksDiscipline.Science: return "cyan"
+  }
+}
+
+export default function ThemeBadge({ children, discipline }: { children: React.ReactNode, discipline: WorksDiscipline }) {
+  return <span className={clsx(style.wrapper, getColor(discipline))}>{children}</span>;
 }
